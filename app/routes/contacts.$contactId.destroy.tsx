35b314e1d2c8f848eab99107265080e8ca2f12ba
AnthonyScott <anthony.scott1005@gmail.com>
2024-09-07 16:43:43 -0400

    Add delete route for Contacts

---
+++ app/routes/contacts.$contactId.destroy.tsx
@@ -0,0 +1,11 @@
+import { redirect, type ActionFunctionArgs } from "@remix-run/node"
+import invariant from "tiny-invariant"
+import { deleteContact } from "../data"
+
+export const action = async ({
+    params,
+}: ActionFunctionArgs) => {
+    invariant(params.contactId, "Missing contactId params")
+    await deleteContact(params.contactId);
+    return redirect(`/`)
+}
